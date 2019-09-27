c3ffb17989d628f8445feec9eb7c678fffe474ca
Adrian <ametch80@harrisonburg.k12.va.us>
2019-09-27 13:10:18 -0400

    I made the code randomly select the assets

--- main.lua
+++ main.lua
@@ -1,6 +1,10 @@
+
+body = { 'pro (1).png', 'pro2.png', 'pro red.png', 'pro redh.png'}
+arms = {'armsup.png', 'armsdown.png', 'armsupdownred.png','armsupdown.png'}
+
 function love.load()
-  man = love.graphics.newImage('pro (1).png')
-  arm = love.graphics.newImage('armsup.png')
+  bodies = love.graphics.newImage(body[love.math.random(1,#body)])
+  arm = love.graphics.newImage(arms[love.math.random(1,#arms)])
 end
 
 function love.update(dt)
@@ -9,6 +13,6 @@ end
 
 
 function love.draw()
-love.graphics.draw(man, 300, 300)
+love.graphics.draw(bodies, 300, 300)
 love.graphics.draw(arm, 300, 300)
 end
