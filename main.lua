de0d241ca3ff4975c543967062b1ec397c260262
Adrian <ametch80@harrisonburg.k12.va.us>
2019-10-03 13:05:09 -0400

    Better procedural gen

--- main.lua
+++ main.lua
@@ -1,10 +1,23 @@
+path  = 'Kenney_characterPack_1/PNG/Skin/Tint 1/'
+path1 = 'Kenney_characterPack_1/PNG/Face/Completes/'
+path2 = 'Kenney_characterPack_1/PNG/Hair/Black/'
 
-body = { 'pro (1).png', 'pro2.png', 'pro red.png', 'pro redh.png'}
-arms = {'armsup.png', 'armsdown.png', 'armsupdownred.png','armsupdown.png'}
+Skins = {path.. 'tint1_head.png'}
+Faces = {path1..'face1.png',path1..'face2.png',path1..'face3.png',path1.. 'face4.png'}
+Hairs =  {path2..'blackMan1.png',path2..'blackMan2.png', path2..'blackMan3.png'}
 
 function love.load()
-  bodies = love.graphics.newImage(body[love.math.random(1,#body)])
-  arm = love.graphics.newImage(arms[love.math.random(1,#arms)])
+  face = love.graphics.newImage(Faces[love.math.random(1,#Faces)])
+  hair = love.graphics.newImage(Hairs[love.math.random(1,#Hairs)])
+  skin = love.graphics.newImage(Skins[love.math.random(1,#Skins)])
+
+
+  skinCenterX = skin:getWidth()/2
+  skinCenterY = skin:getHeight()/2
+
+  faceX = face:getWidth()/2
+  faceY = face:getHeight()/2
+
 end
 
 function love.update(dt)
@@ -13,6 +26,8 @@ end
 
 
 function love.draw()
-love.graphics.draw(bodies, 300, 300)
-love.graphics.draw(arm, 300, 300)
+love.graphics.draw(skin, 400 - skinCenterX, 300 - skinCenterY)
+love.graphics.draw(face, 400 - faceX, 300 - faceY)
+love.graphics.draw(hair, 280, 230)
+
 end
